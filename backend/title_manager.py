d180314a41a2da0f65e6125ae864b383be85ae2a
chris_web <i22037@hb.dhbw-stuttgart.de>
2025-05-24 23:20:49 +0200

    Adelstitel und Akademische Titel hinzugefügt
    - jeweils 20 Adelstitel und Akademische Titel hinzugefügt
    - title_manager: fehler das er die constants liste zweimal eingelesen hat
    - html Sprache in Nationalität abgeändert

--- backend/title_manager.py
+++ backend/title_manager.py
@@ -3,11 +3,6 @@ from .constants import TITEL_METADATA
 class TitleManager:
     def __init__(self):
         self.titles  = list(TITEL_METADATA.keys())
-        self.add_default_titles()
-    
-    def add_default_titles(self):
-        for title in TITEL_METADATA:
-            self.titles.append(title)
 
     def add_title(self, new_title: str, gender: str = "0", nation: str = "DE"):
         if new_title not in self.titles:
